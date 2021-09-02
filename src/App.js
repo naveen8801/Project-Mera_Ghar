94de893bf69aeb1f73fab36ffe06ff5e0e42f1ea
naveen8801 <naveensharma10d@gmail.com>
2021-09-02 11:28:55 +0530

    socket.io implemented

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import { Link, Switch, Route, Redirect } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Navbar from './components/Navbar/Navbar';
 import AdminLoginPage from './pages/AdminLoginRegister/AdminLoginPage';
+import socketIOClient, { io } from 'socket.io-client';
+
+const ENDPOINT = 'http://127.0.0.1:5000';
 
 function App() {
+  const socket = socketIOClient(ENDPOINT, {
+    transports: ['websocket', 'polling', 'flashsocket'],
+  });
+  useEffect(() => {
+    socket.on('Public-Data-Fetched', (data) => {
+      console.log(data);
+    });
+  }, []);
   return (
     <>
       <Navbar />
